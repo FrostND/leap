a0484c97411c93ea92936d6d150e7ea8cf8e1b91
Nick Frost <drnickfrost@gmail.com>
2026-08-25 11:13:40 -0500

    update

--- inst/figures/build_hex.R
+++ inst/figures/build_hex.R
@@ -1,9 +1,12 @@
 
 
 
+# format = .png; for pdf see ?rsvg_pdf
 rsvg::rsvg_png(
   svg = "inst/figures/leap_hex_vector.svg",
-  file = "man/figures/leap_hex.png",
+  file = "inst/figures/leap_hex.png",
   width = 1200,
   height = 1200
 )
+
+
